c6cfe2830ff0858022aefc3e71deb15ebeba65ed
jeevan paudel <jeevanpaudel77@gmail.com>
2021-07-03 13:44:57 +0545

    adding test result and test summary

--- Write_File_Automation.py
+++ Write_File_Automation.py
@@ -5,7 +5,6 @@ from datetime import datetime
 time = str(datetime.now())
 
 test_result_location = 'Output_Result/test_result/TestResult.xlsx'
-test_summary_location = 'Output_Result/test_result/TestSummmary.xlsx'
 
 def excel_creater():
     if(os.path.exists(test_result_location)):
@@ -33,19 +32,16 @@ def write_result(sn,test_summary,result,remarks):
     workbook.save(test_result_location)
 
 def write_summary():
-    workbook = openpyxl.Workbook()
+    # workbook = openpyxl.Workbook()
+    workbook = openpyxl.load_workbook(test_result_location)
     worksheet = workbook.create_sheet('TestSummary')
     worksheet.cell(row=1, column=1).value = "Test Executed On"
     worksheet.cell(row=1, column=2).value = time
     worksheet.cell(row=2, column=1).value = "Number of Test Cases"
-    worksheet.cell(row=2, column=2).value = "=COUNTA(Sheet1!A:A)"
-    workbook.save(test_summary_location)
+    worksheet.cell(row=2, column=2).value = "=(COUNTA(TestResults!A:A) - 1)"
+    workbook.save(test_result_location)
 
 def remove_file():
     if (os.path.exists(test_result_location)):
         os.remove(test_result_location)
-
-    if (os.path.exists(test_summary_location)):
-        os.remove(test_summary_location)
-
 
