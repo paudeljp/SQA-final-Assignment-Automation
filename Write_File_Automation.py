4d43c57382bb8a685a1c495b026cb27429e97a0d
jeevan paudel <jeevanpaudel77@gmail.com>
2021-07-03 13:10:44 +0545

    ..

--- Write_File_Automation.py
+++ Write_File_Automation.py
@@ -5,15 +5,16 @@ from datetime import datetime
 time = str(datetime.now())
 
 test_result_location = 'Output_Result/test_result/TestResult.xlsx'
+test_summary_location = 'Output_Result/test_result/TestSummmary.xlsx'
 
 def excel_creater():
     if(os.path.exists(test_result_location)):
         workbook = openpyxl.load_workbook(test_result_location)
-        worksheet = workbook['Sheet1']
+        worksheet = workbook['TestResults']
         return workbook,worksheet
     else:
         workbook = openpyxl.Workbook()
-        worksheet = workbook.create_sheet('Sheet1')
+        worksheet = workbook.create_sheet('TestResults')
         worksheet.cell(row=1, column=1).value = "SN"
         worksheet.cell(row=1, column=2).value = "Test Summary"
         worksheet.cell(row=1, column=3).value = "Result"
@@ -33,15 +34,18 @@ def write_result(sn,test_summary,result,remarks):
 
 def write_summary():
     workbook = openpyxl.Workbook()
-    worksheet = workbook.create_sheet('Sheet2')
+    worksheet = workbook.create_sheet('TestSummary')
     worksheet.cell(row=1, column=1).value = "Test Executed On"
     worksheet.cell(row=1, column=2).value = time
     worksheet.cell(row=2, column=1).value = "Number of Test Cases"
-    worksheet.cell(row=1, column=4).value = "=COUNTA(Sheet1!A:A)"
-    workbook.save(test_result_location)
+    worksheet.cell(row=2, column=2).value = "=COUNTA(Sheet1!A:A)"
+    workbook.save(test_summary_location)
 
 def remove_file():
     if (os.path.exists(test_result_location)):
         os.remove(test_result_location)
+
+    if (os.path.exists(test_summary_location)):
+        os.remove(test_summary_location)
 
 
